7865b784dd9bdf2f3637b282ae1accf6a952abc3
Grubus <67104183+Grubus@users.noreply.github.com>
2022-01-04 00:00:58 +0100

    commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
+
+app.get("/", function(req, res) {
+    res.send("<h1>my app on heroku!</h1>")
+})
+
+app.get("/data", function(req, res) {
+    res.send({ imie: "Miłosz", nazwisko: "Bebak-Gołąb", klasa: "3I2", grupa: "a" })
+})
+
+app.listen(PORT, function() {
+    console.log("start serwera na porcie " + PORT)
+})
